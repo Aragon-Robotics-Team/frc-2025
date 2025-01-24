732112e24dc4aa62a5c19fe3c48be89d112be3f0
celloii <emmasakai5@gmail.com>
2025-01-23 17:00:14 -0800

    complete Pivot

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -7,18 +7,24 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PivotConstants;
 
 public class Pivot extends SubsystemBase {
   private SparkMax m_leftPivotMotor = new SparkMax(PivotConstants.kLeftPivotMotorID, MotorType.kBrushless);
   private SparkMax m_rightPivotMotor = new SparkMax(PivotConstants.kRightPivotMotorID, MotorType.kBrushless);
+  private DutyCycleEncoder m_encoder = new DutyCycleEncoder(0);
   /** Creates a new Pivot. */
   public Pivot() {}
 
   public void setPivotSpeed(double speed) {
     m_leftPivotMotor.set(speed);
-    m_rightPivotMotor.set(speed);
+    m_rightPivotMotor.set(-speed);
+  }
+
+  public double getPivotPosition(){
+    return m_encoder.get();
   }
 
   @Override
