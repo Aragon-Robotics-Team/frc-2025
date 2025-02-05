ded8e098216a0231b389b0e7c198182b1ad82eb9
deltoidal-icositetrahedron <86028471+deltoidal-icositetrahedron@users.noreply.github.com>
2025-02-04 19:10:35 -0800

    Arm code complete with limit switch commented out

--- src/main/java/frc/robot/commands/ArmToPosition.java
+++ src/main/java/frc/robot/commands/ArmToPosition.java
@@ -1,47 +1,29 @@
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Motor;
 
-public class Spin extends Command {
-  /** Creates a new Spin. */
-  private Motor m_motor;
-  private Joystick m_joystick;
-  private double speed;
-  private final double kSpeedMultiplier = 0.05;
-
-  public Spin(Joystick joystick, Motor motor) {
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ArmToPosition extends Command {
+  /** Creates a new ArmToPosition. */
+  public ArmToPosition() {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_motor = motor;
-    m_joystick = joystick;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    m_motor.setMotorSpeed(0);
-
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    speed = m_joystick.getRawAxis(1) * kSpeedMultiplier;
-    m_motor.setMotorSpeed(speed);
-    
+  public void execute() {}
 
-  }
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-   m_motor.setMotorSpeed(0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
