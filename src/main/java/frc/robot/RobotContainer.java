ef9df5e7e0fccc079b6936c2c610d51bc5b0c865
Remy Chow <Remy.Chow@gmail.com>
2025-02-10 18:09:55 -0800

    pivot code with working simulation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.commands.ArcadeArm;
 import frc.robot.commands.ArcadeElevator;
+import frc.robot.commands.ArcadePivot;
 import frc.robot.commands.ArmToPos;
 import frc.robot.commands.ElevatorPosition;
 import frc.robot.commands.ElevatorRatioTest;
@@ -55,7 +56,7 @@ public class RobotContainer {
   private Pivot m_pivot = new Pivot();
   private double m_speed = 0.7; //change later
   private PivotToPosition m_pivotToA = new PivotToPosition(m_pivot, m_speed, 0);
-  private PivotToPosition m_pivotToB = new PivotToPosition(m_pivot, m_speed, 1);
+  private PivotToPosition m_pivotToB = new PivotToPosition(m_pivot, m_speed, 10);
   private PivotToPosition m_pivotToC = new PivotToPosition(m_pivot, m_speed, 2);
   private JoystickButton m_pivotButtonToA = new JoystickButton(m_secondJoystick, 3);
   private JoystickButton m_pivotButtonToB = new JoystickButton(m_secondJoystick, 1);
@@ -106,6 +107,7 @@ public class RobotContainer {
 
   private JoystickButton m_elevatorRatioTestButtonThree = new JoystickButton(m_secondJoystick, 2);
   private ElevatorRatioTest m_elevatorRatioTestthree = new ElevatorRatioTest(m_elevator, 0.45);
+  private ArcadePivot m_arcadePivot = new ArcadePivot(m_pivot, m_secondJoystick);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -156,7 +158,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     //return m_autoChooser.getSelected();
-    return null;
+    return null; //temporary
   }
 
   private void getTeleopCommand() {
--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -9,10 +9,8 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.simulation.DutyCycleEncoderSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PivotConstants;
@@ -20,39 +18,39 @@ import frc.robot.Constants.PivotConstants;
 public class Pivot extends SubsystemBase {
   private SparkMax m_leftPivotMotor = new SparkMax(PivotConstants.kLeftPivotMotorID, MotorType.kBrushless);
   private SparkMax m_rightPivotMotor = new SparkMax(PivotConstants.kRightPivotMotorID, MotorType.kBrushless);
-  //private DCMotorSim m_pivotSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNEO(2), 0.001, 10), DCMotor.getNEO(2));//ask design for gear ratio
+  private DCMotorSim m_pivotSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNEO(2), 0.001, 10), DCMotor.getNEO(2));//ask design for gear ratio
 
   private Encoder m_encoder = new Encoder(0, 1);
-  //private EncoderSim m_encoderSim = new EncoderSim(m_encoder);
+  private EncoderSim m_encoderSim = new EncoderSim(m_encoder);
   /** Creates a new Pivot. */
   public Pivot() {
     m_encoder.setDistancePerPulse(0.01);
-    //m_encoderSim.setDistancePerPulse(0.01);
+    m_encoderSim.setDistancePerPulse(0.01);
   }
 
   public void setPivotSpeed(double speed) {
     m_leftPivotMotor.set(speed);
     m_rightPivotMotor.set(-speed);
 
-    //m_pivotSim.setInputVoltage(speed * 12);
+    m_pivotSim.setInputVoltage(speed * 12);
   }
 
-  /*public double getSimPivotPosition() {
+  public double getSimPivotPosition() {
     return m_pivotSim.getAngularPositionRotations();
-  }*/
+  }
 
   public double getPivotPosition() {
     return m_encoder.get();
   }
 
-  /*public DCMotorSim getPivotSim() {
+  public DCMotorSim getPivotSim() {
     return m_pivotSim;
-  }*/
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    //m_pivotSim.setInputVoltage(m_pivotSim.getInputVoltage());
-    //m_pivotSim.update(0.02);
+    m_pivotSim.setInputVoltage(m_pivotSim.getInputVoltage());
+    m_pivotSim.update(0.02);
   }
 }
