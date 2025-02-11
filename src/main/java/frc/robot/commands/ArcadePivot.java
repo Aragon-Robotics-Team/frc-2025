1268e2c152923c44d87b83b8d2fa6dca8f27002e
Remy Chow <Remy.Chow@gmail.com>
2025-02-10 18:09:55 -0800

    pivot code with working simulation

--- src/main/java/frc/robot/commands/ArcadePivot.java
+++ src/main/java/frc/robot/commands/ArcadePivot.java
@@ -4,23 +4,19 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Pivot;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class PivotToPosition extends Command {
+public class ArcadePivot extends Command {
   private Pivot m_pivot;
-  private double m_speed;
-  private double m_goal;
-  private double m_currentPosition;
-  private double m_initialPosition;
-
+  private Joystick m_joystick;
   /** Creates a new ArcadePivot. */
-  public PivotToPosition(Pivot pivot, double speed, double goal) {
+  public ArcadePivot(Pivot pivot, Joystick joystick) {
     m_pivot = pivot;
-    m_speed = speed;
-    m_goal = goal;
+    m_joystick = joystick;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_pivot);
   }
@@ -29,32 +25,25 @@ public class PivotToPosition extends Command {
   @Override
   public void initialize() {
     //m_pivot.setPivotSpeed(0);
-    //m_initialPosition = m_pivot.getSimPivotPosition();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_pivot.setPivotSpeed(m_speed);
-    SmartDashboard.putNumber("Pivot Speed", m_speed);
+    m_pivot.setPivotSpeed(m_joystick.getRawAxis(0));
+    SmartDashboard.putNumber("Pivot Speed", m_joystick.getRawAxis(0));
     SmartDashboard.putNumber("Rotations", m_pivot.getSimPivotPosition());
-    
-    m_pivot.getPivotSim().setInputVoltage(12 * m_speed);
-    m_pivot.getPivotSim().update(0.02);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // m_pivot.setPivotSpeed(0);
+    //m_pivot.setPivotSpeed(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if((m_pivot.getSimPivotPosition() - m_goal > -0.3) && (m_pivot.getSimPivotPosition() - m_goal < 0.3)) {
-      return true;
-    }
     return false;
   }
 }
