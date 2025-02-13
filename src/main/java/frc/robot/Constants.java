72765105e29c65157dae55b5929e01cd401a513f
Diya G <diya.gourineni@gmail.com>
2025-02-13 15:52:56 -0800

    new changes by Alex

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,9 +15,9 @@ package frc.robot;
 public final class Constants {
    public final class ElevatorConstants{
     public static final int kElevatorYAxis = 0;
-    public static final double kElevatorMultiplier = 0.1;
+    public static final double kElevatorMultiplier = 0.2;
     public static final int deviceId = 19;
-    public static final int deviceId2=24;
+    public static final int deviceId2 = 24;
     public static final int limitSwitchDio=0;
 
 
