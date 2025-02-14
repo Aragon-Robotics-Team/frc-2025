537390b9d214c3fc6a37c1cbcb7fee3a060070f0
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-02-13 18:30:22 -0800

    Added robot container + more fixes

--- src/main/java/frc/robot/commands/ArmToPos.java
+++ src/main/java/frc/robot/commands/ArmToPos.java
@@ -28,7 +28,6 @@ public class ArmToPos extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_pid.setTolerance(m_pos);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
