1657b5462a7b3ec0010f3fd121be1c9124835cc3
martinying <5122842+martinying@users.noreply.github.com>
2025-02-18 19:20:52 -0800

    Fix compilation errors post rebase.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -100,8 +100,6 @@ public class Robot extends LoggedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
-    m_robotContainer.getTeleopCommand();
   }
 
   /** This function is called periodically during operator control. */
