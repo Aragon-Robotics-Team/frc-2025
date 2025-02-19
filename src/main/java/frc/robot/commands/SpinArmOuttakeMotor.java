a7a88dfa3748656b382c0d03ed2686d995668a40
Diya G <diya.gourineni@gmail.com>
2025-02-18 18:42:32 -0800

    added end code by setting speed to 0

--- src/main/java/frc/robot/commands/SpinArmOuttakeMotor.java
+++ src/main/java/frc/robot/commands/SpinArmOuttakeMotor.java
@@ -33,7 +33,9 @@ public class SpinArmOuttakeMotor extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_arm.spinArmOuttakeMotor(0);
+  }
 
   // Returns true when the command should end.
   @Override
