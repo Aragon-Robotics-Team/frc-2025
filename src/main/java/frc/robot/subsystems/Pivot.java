2bedd90cec6cbeddc664f93793041ab198ef86cd
Remy Chow <Remy.Chow@gmail.com>
2025-02-19 16:09:15 -0800

    pivot testing changes 02/19

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -12,8 +12,12 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.DigitalGlitchFilter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.DutyCycleSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PivotConstants;
@@ -22,22 +26,37 @@ public class Pivot extends SubsystemBase {
   private SparkMax m_pivotMotor = new SparkMax(PivotConstants.kPivotMotorID, MotorType.kBrushless);
   //private DCMotorSim m_pivotSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNEO(PivotConstants.kNumMotors), 0.001, PivotConstants.kGearRatio), DCMotor.getNEO(PivotConstants.kNumMotors));//ask design for gear ratio
 
-  private Encoder m_encoder = new Encoder(PivotConstants.kEncoderChannelA, PivotConstants.kEncoderChannelB);
+  // with this offset, when our pivot is fully down, that is at "1" rotation (0.999)
+  // when our pivot is fully up, that is at ~0.672 rotations
+  private DutyCycleEncoder m_encoder = new DutyCycleEncoder(PivotConstants.kPivotEncoderChannel, 1, 0.1385);
+
+  private DigitalInput topLimitSwitch = new DigitalInput(PivotConstants.kTopLimitSwitchID); // 6
+  private DigitalInput bottomLimitSwitch = new DigitalInput(PivotConstants.kBottomLimitSwitchID); // 8
+
 
   private SparkMaxConfig brakeMode = new SparkMaxConfig();
-  //private EncoderSim m_encoderSim = new EncoderSim(m_encoder);
+
   /** Creates a new Pivot. */
   public Pivot() {
     brakeMode.idleMode(SparkBaseConfig.IdleMode.kBrake);
     m_pivotMotor.configure(brakeMode, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-    m_encoder.setDistancePerPulse(0.01);
-    //m_encoderSim.setDistancePerPulse(0.01);
   }
 
   public void setPivotSpeed(double speed) {
-    m_pivotMotor.set(speed);
+    // negative speed -- move pivot up
+    // positive speed -- move pivot down (to intake)
+    m_pivotMotor.set(-speed);
 
     //m_pivotSim.setInputVoltage(speed * 12);
+  }
+
+
+  public boolean getTopLimitSwitch(){
+    return !topLimitSwitch.get();
+  }
+
+  public boolean getBottomLimitSwitch(){
+    return !bottomLimitSwitch.get();
   }
 
   /*public double getSimPivotPosition() {
