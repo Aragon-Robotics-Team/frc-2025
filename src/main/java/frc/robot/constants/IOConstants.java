34c5e968d8ebf092f6f759fd3b9b737811a37934
Remy Chow <Remy.Chow@gmail.com>
2025-02-22 11:05:22 -0800

    02/20 changes vision

--- src/main/java/frc/robot/constants/IOConstants.java
+++ src/main/java/frc/robot/constants/IOConstants.java
@@ -9,7 +9,7 @@ public final class IOConstants {
     public static final double kDeadband = 0.05;
 
     public static final int kDriveJoystickId = 0;
-    public static int kJoystickXAxis = 1;
-    public static int kJoystickYAxis = 0;
+    public static int kJoystickXAxis = 0;
+    public static int kJoystickYAxis = 1;
     public static int kJoystickRotAxis = 4;
 }
