723f0d28f0b8df32c7afa25cf8d5b64dedff0861
Remy Chow <Remy.Chow@gmail.com>
2025-02-27 09:11:17 -0800

    Edited L4 auto

--- src/main/java/frc/robot/commands/auto/DriveForwardL4.java
+++ src/main/java/frc/robot/commands/auto/DriveForwardL4.java
@@ -4,27 +4,35 @@
 
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.ElevatorConstants;
 import frc.robot.commands.arm.ArmToPos;
 import frc.robot.commands.arm.SpinEndEffectorMotor;
 import frc.robot.commands.elevator.ElevatorToPosition;
+import frc.robot.constants.ArmConstants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.EndEffector;
+import frc.robot.subsystems.SwerveDrive;
 
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DriveForwardAndPlace extends ParallelCommandGroup {
+public class DriveForwardL4 extends ParallelCommandGroup {
   /** Creates a new DriveForwardAndPlace. */
-  public DriveForwardAndPlace(ArmToPos armToL1, ElevatorToPosition elevatorToGround, MoveForTime moveForTime, SpinEndEffectorMotor endEffectorOuttake) {
+  public DriveForwardL4(SwerveDrive swerve, Arm arm, Elevator elevator, EndEffector endEffector, Joystick secondJoystick) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      armToL1,
-      elevatorToGround,
-      new SequentialCommandGroup(new WaitCommand(1), moveForTime),
-      new SequentialCommandGroup(new WaitCommand(8.5), endEffectorOuttake.withTimeout(1)) // 7s move for time so add an extra half second for buffer
+      new ElevatorToPosition(elevator, ElevatorConstants.kL4ElevatorHeight),
+      new SequentialCommandGroup(new WaitCommand(0.2), new ArmToPos(arm, ArmConstants.kL4ArmTickPosition)),
+      new SequentialCommandGroup(new WaitCommand(1), new MoveForTime(swerve, 8, 0.3, 0, 0)),
+      new SequentialCommandGroup(new WaitCommand(8.5), (new SpinEndEffectorMotor(endEffector, -0.9, isScheduled(), secondJoystick)).withTimeout(1)) // 7s move for time so add an extra half second for buffer
       // add an extra second so that the end effector doesn't outtake forever
     );
   }
